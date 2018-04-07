5a6cee46fad2524cfd8db01ed0f2994b220dca85
Vincent Jacob <vincent.jacob@edu.ece.fr>
2018-04-07 22:33:00 +0200

    base

---
+++ Nurseries/src/main/java/com/controllers/SessionController.java
@@ -0,0 +1,21 @@
+package com.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/")
+public class SessionController {
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String getLoginPage() {
+		return "login";
+	}
+	
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String login() {
+		return "login";
+	}
+
+}
